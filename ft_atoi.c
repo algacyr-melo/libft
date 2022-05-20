d439ee8e515ff4f9750d323249b4f9f173ac3627
Algacyr Melo <algacyr.junior@gmail.com>
2022-05-19 22:06:41 -0300

    format and fix code

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: almelo <almelo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 12:06:17 by almelo            #+#    #+#             */
-/*   Updated: 2022/05/14 13:11:26 by almelo           ###   ########.fr       */
+/*   Updated: 2022/05/19 22:01:35 by almelo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ static int	ft_isspace(int c);
 
 static int	ft_isspace(int c)
 {
-	return (c == '\t' ||
-		   	c == '\n' ||
-		   	c == '\v' ||
-		   	c == '\f' ||
-		   	c == '\r' ||
-		   	c == ' ');
+	return (c == '\t'
+		||c == '\n'
+		||c == '\v'
+		||c == '\f'
+		||c == '\r'
+		||c == ' ');
 }
 
 int	ft_atoi(const char *str)
